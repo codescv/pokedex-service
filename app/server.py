5b0a502061ce6a7855b74b05f77117d0e770df6c
Chi Zhang <chi.zhang@codescv.com>
2019-08-09 12:42:11 +0800

    add cn name

--- app/server.py
+++ app/server.py
@@ -10,6 +10,8 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
+import pandas as pd
+
 logger = logging.getLogger(__name__)
 
 model_file_url = 'https://storage.googleapis.com/swift-district-235306.appspot.com/pokedex/models/export.pkl'
@@ -19,6 +21,11 @@ path = Path(__file__).parent
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
+
+pokemon_stats = pd.read_csv(path/'models'/'pokemon_stat.csv')
+e2c = {}
+for e_name, c_name in zip(pokemon_stats['English'], pokemon_stats['Simplified Chinese']):
+  e2c[e_name] = c_name
 
 async def download_file(url, dest):
     if dest.exists(): return
@@ -48,9 +55,9 @@ async def analyze(request):
     data = await request.form()
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    cat = learn.predict(img)[0]
+    cat = learn.predict(img)[0].obj
     logger.info('analyze result: %s', cat)
-    return JSONResponse({'result': cat.obj})
+    return JSONResponse({'result': cat + ' ' + e2c[cat]})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8080)
